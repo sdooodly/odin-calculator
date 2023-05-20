1d034e3c5b1b80000748d7ab33dda2aff0256a1b
Gayathri.Sujatha <Gayathri.Sujatha@arup.com>
2023-05-20 22:27:24 +0530

    step 1

---
+++ script.js
@@ -0,0 +1,22 @@
+const add = (num1, num2) => num1 + num2
+const subtract = (num1, num2) => num1 - num2
+const multiply = (num1, num2) => num1 * num2
+const divide = (num1, num2) => num1 / num2
+
+const operate = (num1, operator, num2) => {
+  return operator == '+'
+    ? add(num1, num2)
+    : operator == '-'
+    ? subtract(num1, num2)
+    : operator == '*'
+    ? multiply(num1, num2)
+    : operator == '/'
+    ? divide(num1, num2)
+    : 'error'
+}
+
+console.log('hello')
+console.log(operate(1, '*', 25))
+console.log(operate(1, '+', 25))
+console.log(operate(1, '-', 25))
+console.log(operate(1, '/', 25))
