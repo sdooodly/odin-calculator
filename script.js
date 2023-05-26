386080ed618c5584917e8a00eb80a9b78d088cbe
sdooodly <gayathrigs96@gmail.com>
2023-05-26 21:52:09 +0530

    refurbish

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const plusOrMinus = document.getElementById("plusOrMinus");
 const dot = document.getElementById("dot");
 const equals = document.getElementById("equals");
 
-const toAdd = (num1, num2) => num1 + num2;
-const toSubtract = (num1, num2) => num1 - num2;
-const toMultiply = (num1, num2) => num1 * num2;
-const toDivide = (num1, num2) => num1 / num2;
-const toCent = (num1, num2) => num1 % num2;
+const toAdd = (num1, num2) => (num1 + num2).toFixed(2);
+const toSubtract = (num1, num2) => (num1 - num2).toFixed(2);
+const toMultiply = (num1, num2) => (num1 * num2).toFixed(2);
+const toDivide = (num1, num2) => (num1 / num2).toFixed(2);
+const toCent = (num1, num2) => (num1 % num2).toFixed(2);
 
 const operate = (num1, operator, num2) => operator == '+'
     ? toAdd(num1, num2)
@@ -21,66 +21,32 @@ const operate = (num1, operator, num2) => operator == '+'
     ? toDivide(num1, num2)
     : operator == '%'
     ? toCent(num1, num2)
-    : 'error';
-
-// console.log(operate(1, '*', 25))
+    : 'Ayo error';
 
 AC.addEventListener("click", () => {
   console.log("click AC gay");
   result.textContent = "";
   })
-  
+
 plusOrMinus.addEventListener("click", () => {
     console.log("click +/- gay");
     const resultNumber = parseFloat(result.textContent);
     result.textContent = -Math.sign(resultNumber) * resultNumber;
   });
 
-// number.forEach((element) => {
-//   element.addEventListener("click", () => {
-//     console.log("click +/- gay"+ element.textContent);
-//     result.textContent = element.textContent;
-//     if(num1 == "") num1 = element.textContent;
-//     else num2 = element.textContent;    
-//     numbersFunc(num1, num2);
-//   });
-// })
-
-// operator.forEach((element) => {
-//   element.addEventListener("click", () => {
-//     console.log("click"+ element.textContent);
-//     result.textContent = element.textContent;
-//     operatorSign = element.textContent;  
-//     operatorsFunc(operatorSign);
-//   });
-// })
-
-// const numbersFunc = (num1, num2) => {
-//   const operator = operatorsFunc
-// }
-// //not this way, call operate when = is clicked
-
-// if(!(num1 == "") && !(num2 == "") && !(operatorSign == "")){
-//   operate(num1, operatorSign, num2)
-// }
-
 concat.forEach((element) => {
   element.addEventListener("click", () => {
     console.log("click"+ result.textContent + element.textContent);
-    result.textContent += element.textContent;
-    // if(num1 == "") num1 = element.textContent;
-    // else num2 = element.textContent;    
+    result.textContent += element.textContent;    
   });
 })
 
-equals.addEventListener("click", () => {
-  console.log("= gay");
-  result.textContent = calculate(result.textContent);
-});
+equals.addEventListener("click", () => result.textContent = calculate(result.textContent));
 
 const calculate = (expression) => {
-  const numbersArray = expression.split(/[+-/*%]/).map(parseFloat);
-  const operatorsArray = expression.split('').filter(exp => /[+-/*%]/.test(exp))
+  //const numbersArray = expression.split(/[+-/*%]/).map(parseFloat);
+  const numbersArray = expression.match(/[+-]?\d+(\.\d+)?/g).map(parseFloat);
+  const operatorsArray = expression.split('').filter(exp => /[+-/*%]/.test(exp) && exp !== '.');
   console.log(numbersArray + "numbersarrya");
   console.log(operatorsArray + "operatorsArray");
   let num1 = numbersArray[0], num2 = numbersArray[1], loopResult;
